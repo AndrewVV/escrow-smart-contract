cd52a67503e1cd9d59d7e28e5a64727a3ff7d9ad
AndrewVV <vyakhirevandrew@gmail.com>
2022-09-16 20:04:04 +0200

    feat: add scripts commands; add localhost network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      initialDate: "1970-01-01T00:00:00Z",
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      initialDate: "1970-01-01T00:00:00Z",
+      gasMultiplier: 1.2,
+    }
+  },
   solidity: "0.8.9",
 };
 
