e8b6d37c94b1d81b64bb150062262844dea3de41
AndrewVV <vyakhirevandrew@gmail.com>
2022-09-29 20:06:02 +0200

    feat: add prettier

--- test/Escrow.ts
+++ test/Escrow.ts
@@ -1,17 +1,16 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
 describe.only('Escrow', () => {
-
     const deployFixture = async () => {
         const [owner, otherAccount] = await ethers.getSigners();
-    
-        const Escrow = await ethers.getContractFactory("Escrow");
+
+        const Escrow = await ethers.getContractFactory('Escrow');
         const escrow = await Escrow.deploy();
-    
+
         return { escrow, owner, otherAccount };
-    }
+    };
 
     describe('Deposit', () => {
         describe('Validations', () => {
@@ -21,14 +20,14 @@ describe.only('Escrow', () => {
                 await expect(escrow.deposit(otherAccount.address)).to.be.revertedWith(
                     'Send value is 0, please set value'
                 );
-            })
-        })
+            });
+        });
 
         describe('Events', () => {
             it('Should emit an event on deposit', async () => {
                 const { escrow, otherAccount } = await loadFixture(deployFixture);
                 const amount = 1;
-      
+
                 await expect(escrow.deposit(otherAccount.address, { value: amount }))
                     .to.emit(escrow, 'Deposited')
                     .withArgs(otherAccount.address, amount);
@@ -39,15 +38,13 @@ describe.only('Escrow', () => {
             it('Should deposit the funds to the contract', async () => {
                 const { escrow, owner, otherAccount } = await loadFixture(deployFixture);
                 const amount = 1;
-      
-                await expect(escrow.deposit(otherAccount.address, { value: amount }))
-                    .to.changeEtherBalances(
-                        [owner, escrow],
-                        [-amount, amount]
-                    )
+
+                await expect(
+                    escrow.deposit(otherAccount.address, { value: amount })
+                ).to.changeEtherBalances([owner, escrow], [-amount, amount]);
             });
         });
-    })
+    });
 
     describe('Withdraw', () => {
         describe('Validations', () => {
@@ -57,27 +54,27 @@ describe.only('Escrow', () => {
                 await expect(escrow.withdraw(otherAccount.address)).to.be.revertedWith(
                     'Status verify is false, please ask deposit sender to verify'
                 );
-            })
+            });
 
             it('Should revert with the right error if payee is not msg.sender', async () => {
                 const { escrow, otherAccount } = await loadFixture(deployFixture);
                 const amount = 1;
-                escrow.deposit(otherAccount.address, { value: amount })
+                escrow.deposit(otherAccount.address, { value: amount });
                 await escrow.verify(otherAccount.address);
 
                 await expect(escrow.withdraw(otherAccount.address)).to.be.revertedWith(
                     'Address payee is not address msg.sender, please change the payee'
                 );
-            })
-        })
+            });
+        });
 
         describe('Events', () => {
             it('Should emit an event on withdraw', async () => {
                 const { escrow, otherAccount } = await loadFixture(deployFixture);
                 const amount = 1;
-                escrow.deposit(otherAccount.address, { value: amount })
+                escrow.deposit(otherAccount.address, { value: amount });
                 await escrow.verify(otherAccount.address);
-      
+
                 await expect(escrow.connect(otherAccount).withdraw(otherAccount.address))
                     .to.emit(escrow, 'Withdrawn')
                     .withArgs(otherAccount.address, amount);
@@ -88,16 +85,13 @@ describe.only('Escrow', () => {
             it('Should withdraw the funds to the payee', async () => {
                 const { escrow, otherAccount } = await loadFixture(deployFixture);
                 const amount = 1;
-                escrow.deposit(otherAccount.address, { value: amount })
+                escrow.deposit(otherAccount.address, { value: amount });
                 await escrow.verify(otherAccount.address);
-      
-                await expect(escrow.connect(otherAccount).withdraw(otherAccount.address))
-                    .to.changeEtherBalances(
-                        [escrow, otherAccount],
-                        [-amount, amount]
-                    )
+
+                await expect(
+                    escrow.connect(otherAccount).withdraw(otherAccount.address)
+                ).to.changeEtherBalances([escrow, otherAccount], [-amount, amount]);
             });
         });
-    })
-
-})
+    });
+});
